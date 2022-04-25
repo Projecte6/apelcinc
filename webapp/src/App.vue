74411557687eb7559281c6894aa0ebad1b4d48c0
David <eljefazo42@gmail.com>
2022-04-25 16:01:02 +0200

    S'ha creat la llista de partides i menu #25

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <Login
-    v-if="currentPage == 'login'"
-    v-model:currentPage="currentPage"
-  />
+  <Login v-if="currentPage == 'login'" v-model:currentPage="currentPage" />
+  <Table v-if="secondPage == 'table'" v-model:secondPage="secondPage" />
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  import { io } from 'socket.io-client';
+import { ref } from "vue";
+import { io } from "socket.io-client";
 
-  // Components
-  import Login from './components/Login.vue';
+// Components
+import Login from "./components/Login.vue";
+import Table from "./components/Table.vue";
 
-  const currentPage = ref('login');
-  // const messages = ref(null);
+const currentPage = ref("login");
+// const messages = ref(null);
+const secondPage = ref("table");
 
-  const socket = io('localhost:3210');
+const socket = io("localhost:3210");
 
-  // socket.on('messages', function (data) {
-  //   messages.value = data;
-  // });
+// socket.on('messages', function (data) {
+//   messages.value = data;
+// });
 </script>
