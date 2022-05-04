6f64ae645472f372d05b6a9f3c26eca89346e955
SEOAlexRamon <seoalexramon@gmail.com>
2022-05-04 18:40:07 +0200

    Created the created and deleted room event

--- server/events/room/create.js
+++ server/events/room/create.js
@@ -1,12 +1,12 @@
 export default (debug, rooms, room) => {
-  if (debug) console.log(`[debug] [on] [create-room] (${room})`);
+  // if (debug) console.log(`[debug] [on] [create-room] (${room})`);
   
-  if (!room.startsWith('game-')) {
-    if (debug) console.log(`[debug] [warning] Ignored room because it does not start with 'game-'`);
-    return;
-  }
+  // if (!room.startsWith('game-')) {
+  //   if (debug) console.log(`[debug] [warning] Ignored room because it does not start with 'game-'`);
+  //   return;
+  // }
 
-  rooms.push({ id: room, status: 'waiting' });
+  // rooms.push({ id: room, status: 'waiting' });
 
-  console.log(`[ws] [create-room] ${room} created as room`);
+  // console.log(`[ws] [create-room] ${room} created as room`);
 };
