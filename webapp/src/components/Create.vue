30f258438cbec92003a82dc20083630dd1804dce
George11849 <george11849@gmail.com>
2022-05-04 15:20:38 +0200

    Solve Create View

--- webapp/src/components/Create.vue
+++ webapp/src/components/Create.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="min-h-screen bg-[#06304E]">
-    <div class="flex justify-center h-screen">
-      <div class="mt-20 overflow-y-auto bg-[#C4C4C4] rounded-lg w-1/2 h-1/2">
+    <Menu></Menu>
+    <div class="flex justify-center mt-14">
+      <div class="overflow-y-auto bg-[#C4C4C4] rounded-lg w-2/3 h-1/2">
         <div class="py-2 inline-block min-w-full px-6">
           <div class="overflow-hidden">
             <h1 class="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-dark text-center mt-12">Nova partida</h1>
           </div>
-          <div class="flex justify-center mt-24">
+          <div class="flex justify-center mt-16 flex-col items-center">
             <div class="bg-slate-600 p-8 w-1/2 rounded-md ">
               <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nom de la partida">
             </div>
-            <div class="flex items-center"><button @click="" class="rounded-md bg-[#585858] text-white font-bold text-xl p-3 ml-4">Crear partida</button></div>
+            <div class="flex items-center"><button @click="" class="rounded-md bg-[#585858] text-white font-bold text-xl p-3 mt-4 mb-4 "><p class="text-white hover:text-yellow-100 ease-in-out duration-200" >Crear partida</p></button></div>
           </div>
         </div>
       </div>
