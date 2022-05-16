2cae57ddc14c9b27a2f439108f7d5760e761ada6
David <eljefazo42@gmail.com>
2022-05-16 23:54:02 +0200

    Created a private room modal and menu and modalrules modified #133

--- webapp/src/components/Join.vue
+++ webapp/src/components/Join.vue
@@ -1,52 +1,79 @@
 <template>
-    <div class="flex md:flex-row items-center pb-8 flex-col">
-    <div class="basis-11/12 flex justify-center w-full">
+  <div class="flex md:flex-row items-center pb-8 flex-col">
+    <div class="basis-11/12 flex justify-center w-full relative">
+      <div v-if="isOpen" class="absolute w-96 h-96 md:left-2/4 bg-[#3f474d] rounded-xl">
+        <div class="flex justify-end pr-2 pt-2">
+          <button @click="isOpen = false"><svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9 text-white hover:bg-[#757575] rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </button>
+        </div>
+        <div class="flex flex-col justify-center items-center h-80">
+          <div class="flex flex-col gap-4">
+            <input v-on:keyup.enter="onClickLogin" class="p-3 font-semibold text-2xl text-center uppercase border-none rounded focus:ring-0" placeholder="#V1f43" />
+            <button class="px-4 py-1 font-medium text-2xl uppercase bg-stone-300 rounded hover:opacity-90" @click="onClickLogin">
+              Accedir
+            </button>
+          </div>
+        </div>
+      </div>
       <div class="overflow-y-auto h-[28rem] bg-[#C4C4C4] rounded-lg w-10/12 mt-8">
         <div class="py-2 inline-block px-6 w-full">
-          <h1 class="text-center font-extrabold text-4xl mb-2">SALES</h1>
-            <table class="w-full">
-              <thead class="border-b">
-                <tr>
-                  <th
-                    v-for="column in columns"
-                    class="text-lg font-bold text-gray-900 px-6 py-4"
-                    v-text="column"
-                  ></th>
-                </tr>
-              </thead>
-              <tbody class="text-center">
-                <tr v-if="!roomsList.length">
-                  <td colspan="4">No hay ninguna partida en espera...</td>
-                </tr>
-                <tr
-                  v-for="room in roomsList"
-                  class="border-b hover:bg-gray-700 hover:text-white"
-                  @click="onClickJoinRoom(room.id)"
-                >
-                  <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="`#${room.id}`" />
-                  <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="room.name" />
-                  <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="`${room.players}/4`" />
-                  <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="'Publica'" />
-                </tr>
-              </tbody>
-            </table>
+          <div class="flex justify-between border-b-4 mt-4 mb-2 pb-6">
+            <div class="flex w-4/6 justify-end">
+              <h1 class="text-center font-extrabold text-4xl mb-2">SALES PÚBLIQUES</h1>
+            </div>
+            <div class="flex w-36 justify-end ml-6">
+              <button @click="isOpen = true" class="rounded-md bg-[#585858] text-white font-bold text-md p-1 w-24 hover:bg-opacity-80">Sales Privades</button>
+            </div>
+          </div>
+          <table class="w-full">
+            <thead class="border-b">
+              <tr>
+                <th
+                  v-for="column in columns"
+                  class="text-lg font-bold text-gray-900 px-6 py-4"
+                  v-text="column"
+                ></th>
+              </tr>
+            </thead>
+            <tbody class="text-center">
+              <tr v-if="!roomsList.length">
+                <td colspan="4">No hay ninguna partida en espera...</td>
+              </tr>
+              <tr
+                v-for="room in roomsList"
+                class="border-b hover:bg-gray-700 hover:text-white"
+                @click="onClickJoinRoom(room.id)"
+              >
+                <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="`#${room.id}`" />
+                <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="room.name" />
+                <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="`${room.players}/4`" />
+                <td class="text-sm font-bold px-6 py-4 whitespace-nowrap" v-text="'Publica'" />
+              </tr>
+            </tbody>
+          </table>
         </div>
-        </div>
-      </div>
-      <div class="basis-1/12 flex">
-      <Chat :socket="props.socket"></Chat>
       </div>
     </div>
+    <div class="basis-1/12 flex">
+      <Chat :socket="props.socket"></Chat>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import Chat from "../components/Chat.vue";
 import { ref } from 'vue';
 
-
+const isOpen = ref(false);
 const props = defineProps({
   socket: Object,
 });
+
+const onClickLogin = () => {
+
+}
 
 const emit = defineEmits(['update:currentPage']);
 
--- webapp/src/components/Menu.vue
+++ webapp/src/components/Menu.vue
@@ -1,18 +1,18 @@
 <template>
-    <nav class="bg-[#3f474d] flex justify-between items-center px-6 py-4">
-      <div class="flex justify-between">
-        <img src="/public/img/logo.png" class="w-80" />
-      </div>
-      <div class="flex items-center ml-4 mr-16 space-x-2 md:mr-6">
-        <button @click="onClickExit" class="rounded-md bg-[#585858] text-white font-bold text-xl p-1 w-24">Sortir</button>
-        <button @click="isOpen = true" class="rounded-md bg-[#585858] text-white font-bold text-xl p-1 w-24">Regles</button>
-        <teleport to="body">
-          <div class="flex justify-center items-center absolute top-0 left-0 w-full h-full" v-if="isOpen">
-            <modal-rules @close="isOpen = false" />
-          </div>
-        </teleport>
-      </div>
-    </nav>
+  <nav class="bg-[#3f474d] flex justify-between items-center px-6 py-4">
+    <div class="flex justify-between">
+      <img src="/public/img/logo.png" class="w-80" />
+    </div>
+    <div class="flex items-center ml-4 mr-16 space-x-2 md:mr-6">
+      <button @click="onClickExit" class="rounded-md bg-[#585858] text-white font-bold text-xl p-1 w-24 hover:opacity-80">Sortir</button>
+      <button @click="isOpen = true" class="rounded-md bg-[#585858] text-white font-bold text-xl p-1 w-24 hover:opacity-60">Regles</button>
+      <teleport to="body">
+        <div class="flex justify-center items-center absolute top-0 left-0 w-full h-full" v-if="isOpen">
+          <modal-rules @close="isOpen = false" />
+        </div>
+      </teleport>
+    </div>
+  </nav>
 </template>
 
 <script setup>
